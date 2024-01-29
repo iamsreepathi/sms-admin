6d222146dfd019936d029f144b7332e0e958b2f7
abhilash9099 <Abhilashreddychemarla@gmail.com>
2024-01-29 16:57:35 -0600

    added loading skeleton to courses page

--- app/departments/[id]/categories/loading.js
+++ app/departments/[id]/categories/loading.js
@@ -1,5 +1,36 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function TheLoading() {
-  return <Skeleton></Skeleton>;
+  return (
+    <div className="space-y-6">
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+      <hr />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 gap-4">
+        {[...Array(15)].map((i, idx) => (
+          <Card key={idx}>
+            <CardHeader>
+              <Skeleton className="h-4" />
+              <Skeleton className="h-4 " />
+            </CardHeader>
+            <CardContent>
+              <Skeleton className="h-4" />
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
 }
