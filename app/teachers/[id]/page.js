7fff599216a57f888936d776963af9f07a47fd5f
abhilash9099 <Abhilashreddychemarla@gmail.com>
2024-02-19 15:21:42 -0600

    completed teacher detail page

--- app/teachers/[id]/page.js
+++ app/teachers/[id]/page.js
@@ -1,14 +1,84 @@
-import PageTitle from "@/components/page-title";
+import AdditionalDetails from "@/components/additonal-details";
+import { getTeacher } from "./actions";
+import DetailCard from "@/components/detail-card";
+import TheCourses from "./courses";
 
 export const metadata = {
   title: "Teacher Details",
   description: "Teacher details",
 };
 
-export default function TheTeacher() {
+const columns = [
+  {
+    accessorKey: "name",
+    header: "Full Name",
+  },
+  {
+    accessorKey: "email",
+    header: "Email Address",
+  },
+  {
+    accessorKey: "gender",
+    header: "Gender",
+  },
+  {
+    accessorKey: "dob",
+    header: "Date of Birth",
+  },
+  {
+    accessorKey: "father",
+    header: "Father",
+  },
+  {
+    accessorKey: "mother",
+    header: "Mother",
+  },
+  {
+    accessorKey: "admission",
+    header: "Admitted On",
+  },
+  {
+    accessorKey: "subject",
+    header: "Primary Subject",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Created On",
+  },
+];
+
+const addtnlCols = [
+  {
+    accessorKey: "name",
+    header: "Dept. Name",
+  },
+  {
+    accessorKey: "phone",
+    header: "Phone No.",
+  },
+  {
+    accessorKey: "email",
+    header: "Email Address",
+  },
+  {
+    accessorKey: "line1",
+    header: "Address",
+  },
+];
+
+export default async function TheTeacher({ params }) {
+  const teacher = await getTeacher(params.id);
   return (
     <div className="space-y-4">
-      <PageTitle title="Teacher detail page" />
+      <div className="grid grid-cols-1 xl:grid-cols-3 gap-4">
+        <DetailCard detail={teacher} columns={columns} />
+        <AdditionalDetails
+          title="Department Details"
+          cols={addtnlCols}
+          data={teacher.department}
+        />
+      </div>
+      <TheCourses courses={teacher.courses} />
     </div>
   );
 }
---
+++ components/additonal-details.js
@@ -0,0 +1,21 @@
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+
+export default function AdditionalDetails({ title, cols = [], data }) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardContent className="text-sm">
+        <ul className="space-y-1">
+          {cols.map((c, idx) => (
+            <li key={idx} className="flex justify-between">
+              <p className="font-semibold">{c.header}:</p>
+              <p>{data[c.accessorKey]}</p>
+            </li>
+          ))}
+        </ul>
+      </CardContent>
+    </Card>
+  );
+}
